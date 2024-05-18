b68da7505b8d48f04f0366c70218d21ac53ace3e
LinhLe18398 <114391347+LinhLe18398@users.noreply.github.com>
2024-05-18 17:55:08 +0700

    giao dien

--- src/component/product/ProductList.js
+++ src/component/product/ProductList.js
@@ -6,24 +6,45 @@ const ProductList = () => {
 
   useEffect(() => {
     axios
-      .get("http://localhost:8090/api/products/showAll")
+      .get("http://localhost:8090/api/products/findListProductByCategory")
       .then((response) => {
         setProducts(response.data);
+        console.log(response.data);
       })
       .catch((error) => {
         console.error("loi", error);
       });
   }, []);
-
   return (
     <>
-      <h1 className="tittleIphone">Iphone</h1>
-      <div className="product-list">
-        {products.map((product) => (
-          <div key={product.id} className="product-card">
-            <img src={"http://localhost:8090/image/" + product.image} />
-            <p>{product.name}</p>
-            <p>Price: {product.price}₫</p>
+      <div>
+        {products.map((item) => (
+          <div className="grey-text count-tr" key={item.id}>
+            <h1 className="tittleIphone">{item.category.name}</h1>
+            <div className="product-list">
+              {item.products.slice(0, 4).map((product) => (
+                <div key={product.id} className="product-card">
+                  <img
+                    src={"http://localhost:8090/image/" + product.image}
+                    alt={product.name}
+                  />
+                  <p>{product.name}</p>
+                  <p>Price: {product.price}₫</p>
+                </div>
+              ))}
+            </div>
+            {item.products.length > 4 && (
+              <div className="a-category">
+                <a
+                  href={`/category/${item.category.id}`}
+                  className="btn btn-outline-primary custom-blue"
+                >
+                  <span className="button-text">
+                    Xem tất cả {item.category.name}&nbsp;&gt;
+                  </span>
+                </a>
+              </div>
+            )}
           </div>
         ))}
       </div>
